8566066e9c10b9eeaf3e9b8caacd44593c4e45d5
Tomasz Popielarczyk <patrycja.popielarczyk1991@gmail.com>
2025-10-19 13:16:48 +0200

    It's my homework lesson3 - html

--- domManipulation/script.js
+++ domManipulation/script.js
@@ -1,5 +1,22 @@
 
 
-// 1. Dodanie nowego elementu przy ładowaniu strony
+// 1. Dodanie nowego elementu przy ładowaniu 
+
+
+const newDiv = document.createElement('patrycja');
+newDiv.textContent = 'To jest  div patrycja';
+document.body.appendChild(newDiv);
+
 // 2. Zmiana koloru paragrafu
-// 3.
+
+document.querySelector('patrycja').style.color = "red";
+const firstParagraph = document.querySelector('p');
+firstParagraph.style.color = "red";
+
+// 3. Dla chętnych: pobierz element paragrafu nr 2. Pobierz element przycisku i uruchom na nim metodę addEventListener na zdarzenie ‚click’. W metodzie addEventListener zmień kolor przycisku.
+
+//const secondParagraph = document.querySelector("#'paragraph-for-button'");
+const button = document.querySelector("#changeColorBtn");
+button.addEventListener('click', function(){
+    alert("Przycisk klikniety")
+})
